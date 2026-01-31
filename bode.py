766481097795704293895af5a3b9eacc7a53a4d5
Nicola Sperandio <116182203+nsperandio@users.noreply.github.com>
2026-01-31 17:59:39 +0100

    dB gain plot and phase from delay
    - the option to use decibel for gain is added;
    - the phase is derived from the "delay" scope measurement instead of "phase". This is done to achieve more reliable measurements
    - phase wrapping (work in progress)
    - reorganized plot structure (work in progress)

--- bode.py
+++ bode.py
@@ -16,6 +16,18 @@ import matplotlib.pyplot as plt
 
 import scipy.signal
 
+def unwrap_phase_continuous(phases_deg,freq_):
+    #out = np.zeros_like(phases_deg)
+    #out[0] = phases_deg[0]
+    ph_arr = np.unwrap(np.array(phases_deg)*(np.pi/180),discont=np.pi/8,period=np.pi/2)
+    out = list(ph_arr*(180/np.pi))
+    #for i in range(1, len(phases_deg)):
+        #if (abs((phases_deg[i]-phases_deg[i-1]))>50):
+        #    out[i] = -phases_deg[i]
+        #else:
+        #    out[i] = phases_deg[i]
+    return out
+
 parser = argparse.ArgumentParser(description="This program plots Bode Diagrams of a DUT using an JDS6600 and Rigol DS1054Z")
 
 parser.add_argument('MIN_FREQ', metavar='min', type=float, help="The minimum frequency for which should be tested")
@@ -23,7 +35,8 @@ parser.add_argument('MAX_FREQ', metavar='max', type=float, help="The maximum fre
 parser.add_argument('COUNT', metavar='N', nargs="?", default=50, type=int, help='The number of frequencies for which should be probed')
 parser.add_argument("--awg_port", dest="AWG_PORT", default="COM3", help="The serial port where the JDS6600 is connected to")
 parser.add_argument("--ds_ip", default="auto", dest="OSC_IP", help="The IP address of the DS1054Z. Set to auto, to auto discover the oscilloscope via Zeroconf")
-parser.add_argument("--linear", dest="LINEAR", action="store_true", help="Set this flag to use a linear scale")
+parser.add_argument("--linear", dest="LINEAR", action="store_true", help="Set this flag to use a linear time scale")
+parser.add_argument("--dB", dest="DB", action="store_true", help="Set this flag to use a dB y axis scale")
 parser.add_argument("--awg_voltage", dest="VOLTAGE", default=5, type=float, help="The amplitude of the signal used for the generator")
 parser.add_argument("--step_time", dest="TIMEOUT", default=0.00, type=float, help="The pause between to measurements in ms.")
 parser.add_argument("--phase", dest="PHASE", action="store_true", help="Set this flag if you want to plot the Phase diagram too")
@@ -89,7 +102,7 @@ if not args.MANUAL_SETTINGS:
     scope.set_channel_offset(2, 0)
 
     # Set the sensitivity according to the selected voltage
-    scope.set_channel_scale(1, args.VOLTAGE / 3, use_closest_match=True)
+    scope.set_channel_scale(1, args.VOLTAGE / 2, use_closest_match=True)
     # Be a bit more pessimistic for the default voltage, because we run into problems if it is too confident
     scope.set_channel_scale(2, args.VOLTAGE / 2, use_closest_match=True) 
 
@@ -105,14 +118,18 @@ awg.setamplitude(AWG_CHANNEL, AWG_VOLT)
 
 volts = list()
 phases = list()
+gains = list()
 
 # We have to wait a bit before we measure the first value
 awg.setfrequency(AWG_CHANNEL, float(freqs[0]))
-time.sleep(0.05)
+time.sleep(1)
 
 for freq in freqs:
     awg.setfrequency(AWG_CHANNEL, float(freq))
-    time.sleep(TIMEOUT)
+    if(freq<=100):
+        time.sleep(5)#TIMEOUT)
+    else:
+        time.sleep(1)#TIMEOUT)
 
     if not args.NORMALIZE:
         volt = scope.get_channel_measurement(2, 'vpp')
@@ -136,12 +153,19 @@ for freq in freqs:
 
     # Measure phase
     if args.PHASE:
-        phase = scope.get_channel_measurement('CHAN1, CHAN2', 'rphase')
-        if phase:
-            phase = -phase
+        delay = scope.get_channel_measurement('CHAN1, CHAN2', 'fdelay')
+        phase = -360*freq*delay
         phases.append(phase)
+        #phase = scope.get_channel_measurement('CHAN1, CHAN2', 'rphase')
+        #if phase:
+        #    phase = -phase
+        #phases.append(phase)
 
     print(freq)
+
+# Phase unwrapping
+phases = unwrap_phase_continuous(phases, freqs)
+
 
 # Write data to file if needed
 if args.file:
@@ -154,8 +178,10 @@ if args.file:
     for n in range(0, len(freqs)):
         if volts[n]:
             volt = volts[n]
+            gains.append(20*np.log10(volt))
         else:
             volt = float("nan")
+            gains.append(float("nan"))
         
         if args.PHASE:
             if phases[n]:
@@ -173,41 +199,61 @@ if args.file:
 if not args.PLOTS:
     exit()
 
-plt.plot(freqs, volts, label="Measured data")
-if args.SMOOTH:
-    try:
-        yhat = scipy.signal.savgol_filter(volts, 9, 3) # window size 51, polynomial order 3
-        plt.plot(freqs, yhat, "--", color="red", label="Smoothed data")
-    except:
-        print("Error during smoothing amplitude data")
+if not args.DB:
+    plt.plot(freqs, volts, label="Measured data")
+    if args.SMOOTH:
+        try:
+            yhat = scipy.signal.savgol_filter(volts, 9, 3) # window size 51, polynomial order 3
+            plt.plot(freqs, yhat, "--", color="red", label="Smoothed data")
+        except:
+            print("Error during smoothing amplitude data")
 
-plt.title("Amplitude diagram (N=%d)"%STEP_COUNT)
-plt.xlabel("Frequency [Hz]")
-plt.ylabel("Voltage Peak-Peak [V]")
-plt.legend()
+    plt.title("Amplitude diagram (N=%d)"%STEP_COUNT)
+    plt.xlabel("Frequency [Hz]")
+    plt.ylabel("Voltage Peak-Peak [V]")
+    plt.xscale("log")
+    plt.legend()
+    plt.show()
 
+if args.DB and not args.PHASE:
+    plt.plot(freqs, gains, color="blue", label="20*log10(V)")
+    plt.title("Gain Bode (N=%d)"%STEP_COUNT)
+    plt.xlabel("Frequency [Hz]")
+    plt.ylabel("Gain [dB]")
+    plt.show()
 # Set log x axis
 if not args.LINEAR:
     plt.xscale("log")
 
-plt.show()
-
 if args.PHASE:
-    plt.plot(freqs, phases)
-    plt.title("Phase diagram (N=%d)"%STEP_COUNT)
-    plt.ylabel("Phase [°]")
-    plt.xlabel("Frequency [Hz]")
+    if not args.DB:
+        plt.plot(freqs, phases)
+        plt.title("Phase diagram (N=%d)"%STEP_COUNT)
+        plt.ylabel("Phase [°]")
+        plt.xlabel("Frequency [Hz]")
 
-    if args.SMOOTH:
-        try:
-            yhat = scipy.signal.savgol_filter(phases, 9, 3) # window size 51, polynomial order 3
-            plt.plot(freqs, yhat, "--", color="red", label="Smoothed data")
-        except:
-            print("Error during smoothing phase data")
+        if args.SMOOTH:
+            try:
+                yhat = scipy.signal.savgol_filter(phases, 9, 3) # window size 51, polynomial order 3
+                plt.plot(freqs, yhat, "--", color="red", label="Smoothed data")
+            except:
+                print("Error during smoothing phase data")
+        # Set log x axis
+        if not args.LINEAR:
+            plt.xscale("log")
+        plt.show()
+    else:
+        fig, axs = plt.subplots(2, 1, sharex=True)
+        axs[0].plot(freqs, gains, color="red")
+        axs[0].set_ylabel("Gain [dB]")
+        axs[0].set_xscale("log")
+        axs[0].grid(True)
         
+        axs[1].plot(freqs, phases, color="blue")
+        axs[1].plot(freqs, scipy.signal.savgol_filter(phases,9,3), "--", color="red")
+        axs[1].set_ylabel("Phase [°]")
+        axs[1].set_xlabel("Frequency [Hz]")
+        axs[1].set_xscale("log")
+        axs[1].grid(True)
 
-    # Set log x axis
-    if not args.LINEAR:
-        plt.xscale("log")
-
-    plt.show()
+        plt.show()
